62d51628c4b4d99530bf9f0a692990dae71f4437
raj <imraaj10262gmail.comclear>
2025-03-16 14:49:14 +0530

    Updated files

--- sheriyans/backend/script.js
+++ sheriyans/backend/script.js
@@ -34,8 +34,57 @@
 // })
 
 // 2.HTTP,https Server-protocols
-const http = require("http");
-const server = http.createServer(function(req,res){
-    res.end("hello world");
+// const http = require("http");
+// const server = http.createServer(function(req,res){
+//     res.end("hello world");
+// })
+// server.listen(3000);
+
+// 3. Npm basics and advanced
+
+// 1. installing and unistalling
+// npm install express
+// npm uninstall express
+ 
+// installing particular version using @
+// dependencies -packages and package ki dependencies
+// devdependencies -bs development me kaam aaenge app bnne ke baad ni
+// Script- custom and default
+
+// 4. express
+
+// Express is a Framework
+// express js is a npm package
+// manages everything from receiving the request and giving the Response
+
+// setting up basic express application
+// routing
+// const express = require("express");
+// const app = express();
+// app.get("/",function(req,res){
+//     res.send("hello world");
+// })
+// app.get("/profile",function(req,res){
+//     res.send("heelo raj kya k")
+// })
+// app.listen(3000);
+
+// // middleware-routes se phle or accept hone ke baad agr kuch task krawana ho to use hota h
+
+const express = require("express");
+const app = express();
+app.use(function(req,res,next){
+    console.log("middleware-routes");
+    next();
 })
-server.listen(3000);
+// app.get("/",function(req,res){
+//     res.send("hello world");
+// } )
+app.get("/profile",function(req,res,next){
+    return next(new Error("Not implemented"));
+} )
+app.use((err,req,res,next)=>{
+    console.error(err.stack)
+    res.status(500).send("something broke!")
+})
+app.listen(3000);
